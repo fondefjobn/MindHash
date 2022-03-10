ff236d3866e1f5e631c98bda5136a24217995dc4
NevisKing <nevisdontforget@gmail.com>
2022-03-10 15:15:24 +0100

    added documentation template file

---
+++ doc-template.py
@@ -0,0 +1,40 @@
+
+#Module Description Template
+"""
+Responsible for module: <Your name>
+Description: <Longform description of the module, what it serves to do, and why>
+"""
+
+
+# Module Descriptions Example
+"""
+Responsible for module: <Your name>
+Description: <This file exists to act as a template for the documentation of our code. 
+If there are changes you would like to make to it please contact Nevis King.>
+"""
+
+
+# Function Descriptions Template
+"""
+Description: <>
+---------------------------------
+for each input:
+<input name> : <Expected datatype>
+---------------------------------
+<output name> : <Expected datatype>
+"""
+
+
+# Function Description Example
+"""
+Description: <This function adds two numbers together>
+---------------------------------
+someInput : <Expected datatype>
+someOtherInput : <Expected datatype>
+---------------------------------
+someOutput : <Expected datatype>
+"""
+
+def someFunction(someInput, someOtherInput):
+    someOutput = someInput + someOtherInput
+    return someOutput
