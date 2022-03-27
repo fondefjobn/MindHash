b36173509bdac5162b2d41a2e6aeb99eda6bd1d4
Radu Rebeja <r.rebeja@student.rug.nl>
2022-03-28 01:05:15 +0200

    critical commit, Pipeline class removed, replaced with methods bundled in class

--- cli/exec_cli.py
+++ cli/exec_cli.py
@@ -11,6 +11,9 @@ after MVP completion:
 
 def parse_config():
     parser = argparse.ArgumentParser(description='StreetAnalytics Project CLI')
+    parser.add_argument('--sensor', default=None,
+                        help='Sensor defining input type', choices=['ouster'], type=str.lower, required=True)
+    parser.add_argument('--N', type=int, default=None, help='Limit number N of processed frames')
     parser.add_argument('--live', default=None,
                         help='Live-stream processing mode', action="store_true")
     parser.add_argument('--post', default=None,
@@ -19,11 +22,12 @@ def parse_config():
                         help='Evaluate input with a ML Model', action="store_true")
     parser.add_argument('--host', type=str, default=None, help='Sensor hostname')
     parser.add_argument('--port', type=str, default=None, help='Sensor port')
-    parser.add_argument('--ml', type=str, default='PVRCNN', choices=['PVRCNN'],
+    parser.add_argument('--ml', type=str.upper, default='PVRCNN', choices=['PVRCNN'],
                         help='Model name')
     parser.add_argument('--mlpath', type=str, default=None, help='Model path from content root')
-    parser.add_argument('--pcap', type=str, default=None, help='PCAP file for post-processing')
-    parser.add_argument('--meta', type=str, default=None, help='JSON metadata file for post-processing')
+    parser.add_argument('--input', type=str, default=None, help='PCAP/other file for post-processing',
+                        required=True)
+    parser.add_argument('--meta', type=str, default=None, help='JSON metadata file for of input file')
     parser.add_argument('-v', "--verbose", help='Verbose output', action="store_true")
     parser.add_argument('--visual', help='Visualize results with Open3D', action="store_true")
     parser.add_argument('--export', help='Save results locally', action="store_true")
