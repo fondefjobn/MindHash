595893424646dc24acb1f66b55607dfc41e0c620
Radu Rebeja <57535992+d33dler@users.noreply.github.com>
2022-04-04 22:55:53 +0200

    Revert "Mindhash MK1"

--- eval_api/examplecalls/calls.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-
-def makearr():
-    x = numpy.array([10,11,12])
-    numpy.save('data.npy', x)
-
-makearr()
--- statistic/stats.py
+++
@@ -1,9 +0,0 @@
-from tools.pipes import RoutineSet
-
-
-class Routines(RoutineSet):
-    def id(self):
-        return 'STAT_BUNDLE'
-
-    def Statistics(self, state, *args):
-        pass
