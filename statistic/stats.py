a8d90ec9c7484e25bb6221b7e2920b2863a61aa0
joeldvd <jdieguvi15@alumnes.ub.edu>
2022-06-17 22:31:10 +0200

    Little modifications to statistics

--- statistic/stats.py
+++ statistic/stats.py
@@ -30,78 +30,122 @@ class Routines(RNode):
 
 class Statistics:
 
-    def __init__(self, data):  # TODO basic class without reference, super class with reference
+    threshold = [0.7, 0.5, 0.5]
+    object_names = ['Cars', 'Persons', 'Bicycles']
+
+    def __init__(self, data):
+
         self.boxes = data['ref_boxes']
         self.scores = data['ref_scores']
         self.labels = data['ref_labels']
+        self.times = data['ref_time']
 
-        self.totals = [0] * len(object_names)  # count number of occurences for each object type
+        self.totals = [0] * len(self.object_names)  # the number of occurrences for each object type detected
 
     def process(self):
         for score, label in zip(self.scores, self.labels):
-            if score >= threshold:
-                self.totals[label] += 1
+            if score >= self.threshold[label-1]:
+                self.totals[label-1] += 1
 
     def to_json(self):
         output = {}
-        for obj, count in zip(object_names, self.totals):
+        for obj, count in zip(self.object_names, self.totals):
             output[obj] = count
+        output['average_time'] = self.average_detection_time()
+        output['standard_deviation_time'] = stdev(self.times)
         return output
 
+    def average_detection_time(self):
+        return sum(self.times) / len(self.times)
 
-class Statistics_with_Reference(Statistics):
+
+class ReferenceStatistics(Statistics):
+
+    leeway = 0.15             # margin percentage
+    tn = 0             # true negatives (constant 0)
 
     def __init__(self, data, reference):
-        Statistics.__init__(self, data)
-        self.reference = reference  # reference
+        super().__init__(data)
+        self.reference = reference
 
         # Split in 3 categories: 1-car, 2-pedestrian, 3-cyclist
-        self.tp = [0] * len(object_names)  # true positives
-        self.fp = [0] * len(object_names)  # false positives
-        self.fn = [0] * len(object_names)  # false negatives
+        self.tp = [0] * len(self.object_names)  # true positives
+        self.fp = [0] * len(self.object_names)  # false positives
+        self.fn = [0] * len(self.object_names)  # false negatives
+
+    def find_and_compare_box(self, box, margin, left, right, label):
+        for idx in range(left, right):
+            cond = True
+            for i in range(6):   # box dimensions definition
+                if not (cond and self.reference[idx][i] + margin > box[i] >
+                        self.reference[idx][i] - margin):
+                    cond = False
+                    break;
+            if cond:
+                if label == self.reference[idx][6]:
+                    self.tp[self.reference[idx][6]-1] += 1
+                else:
+                    self.fp[self.reference[idx][6]-1] += 1
+                return
+
+        self.fn[label-1] += 1
 
     def process(self):
-        for score, label in zip(self.scores, self.labels):
-            if score >= threshold:
-                self.totals[label] += 1
-        # TODO: reference
+        super().process()
+
+        for label, box in zip(self.labels, self.boxes):
+            margin = self.leeway * sum(box[3:6]) / 3
+            box[0] -= margin
+            left = bisect_left(self.reference, box)
+            box[0] += 2 * margin
+            right = bisect_right(self.reference, box)
+            box[0] -= margin
+
+            self.find_and_compare_box(box, margin, left, right, label)
+
+    def sort_reference(self):
+        self.reference.sort(key=lambda x: x[0])
 
     def to_json(self):
-        output = {}
-        output['count'] = {}
-        for obj, count in zip(object_names, self.totals):
-            output['count'][obj] = count
+        output = super().to_json()
         output['precision'] = {}
-        for i in range(len(object_names)):
-            output['precision'][object_names[i]] = self.precision(i)
+        for i in range(len(self.object_names)):
+            output['precision'][self.object_names[i]] = self.precision(i)
         output['precision']['macro_avg'] = self.macro_avg(self.precision)
         output['precision']['weighted_avg'] = self.weighted_avg(self.precision)
         output['recall'] = {}
-        for i in range(len(object_names)):
-            output['recall'][object_names[i]] = self.recall(i)
+        for i in range(len(self.object_names)):
+            output['recall'][self.object_names[i]] = self.recall(i)
         output['recall']['macro_avg'] = self.macro_avg(self.recall)
         output['recall']['weighted_avg'] = self.weighted_avg(self.recall)
         output['f_score'] = {}
-        for i in range(len(object_names)):
-            output['f_score'][object_names[i]] = self.f_score(i)
+        for i in range(len(self.object_names)):
+            output['f_score'][self.object_names[i]] = self.f_score(i)
         output['f_score']['macro_avg'] = self.macro_avg(self.f_score)
         output['f_score']['weighted_avg'] = self.weighted_avg(self.f_score)
         return output
 
     def precision(self, i):
+        if self.tp[i] + self.fp[i] == 0: return 0
         return self.tp[i] / (self.tp[i] + self.fp[i])
 
     def recall(self, i):
+        if self.tp[i] + self.fn[i] == 0: return 0
         return self.tp[i] / (self.tp[i] + self.fn[i])
 
     def f_score(self, i):
+        if self.precision(i) + self.recall(i) == 0: return 0
         return 2 * self.precision(i) * self.recall(i) / (self.precision(i) + self.recall(i))
 
     def macro_avg(self, function):
+        object_names = self.object_names
         return sum(function(i) for i in range(len(object_names))) / len(object_names)
 
     def weighted_avg(self, function):
+        if sum(self.totals) == 0: return 0
+        object_names = self.object_names
         return sum(function(i) * self.totals[i] for i in range(len(object_names))) / sum(self.totals)
 
     def accuracy(self):
+        if sum(self.totals) == 0: return 0
         return sum(self.tp) / sum(self.totals)
